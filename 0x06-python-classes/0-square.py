d8b8ee4dfbf127c87476b32199966b49db5a6f09
Segun <ojoolusegun17@gmail.com>
2022-12-20 18:35:23 -0800

    an empty class Square that defines a square:

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""
+File: 0-square.py
+Desc: This module contains a single empty class defination.
+Author: Olusegun Ojo (JSPlacid)
+Date Created: Dec 20 2022
+"""
+
+class Square():
+    """
+    This is an empty class created for showing how to create
+    a class and it does nothing.
+    """
+    pass
