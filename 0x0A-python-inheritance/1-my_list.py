787318dcab6be81ae9f84080b20f6f47286f42a0
Segun <ojoolusegun17@gmail.com>
2023-01-09 19:16:01 -0800

    Write a class MyList that inherits from list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-'''Module: 1-my_list
+''' Module: 1-my_list
 '''
 
+
 class MyList(list):
-    """
-     class MyList that inherits from list
-    """
+    ''' Represents a MyList
+    '''
+
     def print_sorted(self):
-        """
-        Public instance method that prints sorted list
-        """
-        list_copy = self[:]
-        list_copy.sort()
-        print(list_copy)
+        '''
+        prints the list, but sorted
+        '''
+        print(sorted(self))
